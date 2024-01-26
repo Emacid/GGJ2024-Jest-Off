7f3f21eb3ffaeb8fd2dfd197a88a4e01c49af604
Onur Varol <115077995+Emacid@users.noreply.github.com>
2024-01-26 22:15:39 +0300

    Arms Done

---
+++ Assets/Onur/Scripts/JointBehaviourLeft.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JointBehaviourLeft : MonoBehaviour
+{
+    public GameObject jointArmLeft;
+
+    void Start()
+    {
+
+    }
+
+    private bool rotate = false;
+    public float rotationSpeed = 300f;
+
+    void Update()
+    {
+        gameObject.transform.rotation *= Quaternion.Euler(0, 0, -250f * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            rotate = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Q))
+        {
+            rotate = false;
+        }
+
+        if (rotate)
+        {
+            float rotationAmount = rotationSpeed * Time.deltaTime;
+            transform.rotation *= Quaternion.Euler(0, 0, rotationAmount);
+        }
+    }
+}
